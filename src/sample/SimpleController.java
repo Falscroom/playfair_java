0788d832a560c2af05045ce2fd788f415a2eb90f
falscroom <falscroom@gmail.com>
2015-03-29 11:28:23 +1000

    Добавлена возможность декодировать текст

--- src/sample/SimpleController.java
+++ src/sample/SimpleController.java
@@ -10,6 +10,7 @@ public class SimpleController {
     public TextField text;
     public TextField coded_text;
     public char[][] alphabet_matrix;
+    public boolean encode = false;
 
      public class cordinate {
         public int i,j;
@@ -20,7 +21,6 @@ public class SimpleController {
             this.simbol = simbol;
         }
     }
-
      public cordinate[] get_cordinate(char a,char b) {
         cordinate[] cord_array = new cordinate[2];
         for(int i = 0;i < 5;i++){
@@ -36,11 +36,7 @@ public class SimpleController {
         }
         return cord_array;
     }
-
     public int which_case(cordinate[] arr) {
-/*        if(arr[0].simbol == arr[1].simbol) { //ДВА ОДИНАКОВЫХ СИМВОЛА! С добавлением функции replace repeats такая ситуация невозможна!
-            return 1;
-        }*/
         if(arr[0].i == arr[1].i) { // В ОДНОЙ СТРОКЕ!
             return 2;
         }
@@ -52,27 +48,56 @@ public class SimpleController {
     public void code_it(int case_num,cordinate[] arr) {
         switch (case_num) {
             case 2:
-                arr[0].j ++;
-                arr[1].j ++;
+                if(encode) {
+                    arr[0].j --;
+                    arr[1].j --;
+                }
+                else {
+                    arr[0].j ++;
+                    arr[1].j ++;
+                }
                 if(arr[0].j > 4) arr[0].j = 0;
                 if(arr[1].j > 4) arr[1].j = 0;
+
+                if(arr[0].j < 0) arr[0].j = 4;
+                if(arr[1].j < 0) arr[1].j = 4;
+
                 for (int i = 0;i < 2;i++)
-                    coded_text.appendText(Character.toString(alphabet_matrix[arr[i].i][arr[i].j]));
+                    if(encode)
+                        text.appendText(Character.toString(alphabet_matrix[arr[i].i][arr[i].j]));
+                    else
+                        coded_text.appendText(Character.toString(alphabet_matrix[arr[i].i][arr[i].j]));
                 break;
             case 3:
-                arr[0].i ++;
-                arr[1].i ++;
+                if(encode) {
+                    arr[0].i --;
+                    arr[1].i --;
+                }
+                else {
+                    arr[0].i ++;
+                    arr[1].i ++;
+                }
                 if(arr[0].i > 4) arr[0].i = 0;
                 if(arr[1].i > 4) arr[1].i = 0;
+
+                if(arr[0].i < 0) arr[0].i = 4;
+                if(arr[1].i < 0) arr[1].i = 4;
+
                 for (int i = 0;i < 2;i++)
-                    coded_text.appendText(Character.toString(alphabet_matrix[arr[i].i][arr[i].j]));
+                    if(encode)
+                        text.appendText(Character.toString(alphabet_matrix[arr[i].i][arr[i].j]));
+                    else
+                        coded_text.appendText(Character.toString(alphabet_matrix[arr[i].i][arr[i].j]));
                 break;
             case 4:
                 int buf_j = arr[0].j;
                 arr[0].j = arr[1].j;
                 arr[1].j = buf_j;
                 for (int i = 0;i < 2;i++)
-                    coded_text.appendText(Character.toString(alphabet_matrix[arr[i].i][arr[i].j]));
+                    if(encode)
+                        text.appendText(Character.toString(alphabet_matrix[arr[i].i][arr[i].j]));
+                    else
+                        coded_text.appendText(Character.toString(alphabet_matrix[arr[i].i][arr[i].j]));
                 break;
         }
     }
@@ -86,16 +111,9 @@ public class SimpleController {
         if(str_b.length() % 2 == 1 ) str_b.insert(str_b.length(),'X');
         return str_b;
     }
-
-
-    public void code_decode(ActionEvent actionEvent) {
-        alphabet_matrix = new char[5][5];
-        StringBuffer alphabet = new StringBuffer("ABCDEFGHIJKLMNOPRSTUVWXYZ".subSequence(0,25));
-        StringBuffer str_key_word = new StringBuffer(key_word.getText().toUpperCase().subSequence(0, key_word.getText().length()));
-
-
+    public void delete_repeats(StringBuffer str_key_word,StringBuffer alphabet) { // Вообще можно сделать, чтобы сразу удалялись пробелы, но мне лень думать.
         for(int i = 0;i<str_key_word.length();i++) {
-            char simbol = str_key_word.charAt(i); 
+            char simbol = str_key_word.charAt(i);
             for(int j = i + 1;j < str_key_word.length();j++) {
                 if(simbol == str_key_word.charAt(j)) {
                     str_key_word.setCharAt(j,' ');
@@ -107,9 +125,8 @@ public class SimpleController {
                 }
             }
         }
-        str_key_word = new StringBuffer(str_key_word.toString().replaceAll(" ","").subSequence(0,str_key_word.toString().replaceAll(" ","").length()));
-        alphabet = new StringBuffer(alphabet.toString().replaceAll(" ","").subSequence(0,alphabet.toString().replaceAll(" ","").length()));
-
+    }
+    public void fill_the_matrix(StringBuffer str_key_word,StringBuffer alphabet){
         for(int i = 0;i<5;i++) {
             for(int j = 0;j<5;j++) {
                 if(i * 5 + j < str_key_word.length())
@@ -118,15 +135,45 @@ public class SimpleController {
                     alphabet_matrix[i][j] = alphabet.charAt(i * 5 + j  - str_key_word.length());
             }
         }
-        StringBuffer our_text = new StringBuffer(text.getText().toUpperCase().replaceAll(" ", "").subSequence(0, text.getText().replaceAll(" ", "").length()));
-        our_text = replace_repeats(our_text);
+    }
+    public void fill_last_input(StringBuffer our_text) {
         cordinate[] cord_arr;
         for(int i = 1;i < our_text.length(); i ++) {
             if((i - 1)%2 == 0) {
-               cord_arr = get_cordinate(our_text.charAt(i - 1),our_text.charAt(i));
-               code_it(which_case(cord_arr),cord_arr);
-           }
+                cord_arr = get_cordinate(our_text.charAt(i - 1),our_text.charAt(i));
+                code_it(which_case(cord_arr),cord_arr);
+            }
+        }
+    }
+    public void encode_it(StringBuffer str) {
+        encode = true;
+        fill_last_input(str);
+        encode = false;
+    }
+    public void ClickButton(ActionEvent actionEvent) {
+        //////////////
+        alphabet_matrix = new char[5][5];
+        StringBuffer alphabet = new StringBuffer("ABCDEFGHIJKLMNOPRSTUVWXYZ".subSequence(0,25));
+        StringBuffer str_key_word = new StringBuffer(key_word.getText().toUpperCase().subSequence(0, key_word.getText().length()));
+        StringBuffer our_text = replace_repeats(new StringBuffer(text.getText().toUpperCase().replaceAll(" ", "").subSequence(0, text.getText().replaceAll(" ", "").length())));
+        /////////////
+        if(our_text.length() > 0) {
+            delete_repeats(str_key_word,alphabet);
+            str_key_word = new StringBuffer(str_key_word.toString().replaceAll(" ","").subSequence(0,str_key_word.toString().replaceAll(" ","").length()));
+            alphabet = new StringBuffer(alphabet.toString().replaceAll(" ","").subSequence(0,alphabet.toString().replaceAll(" ","").length()));
+            fill_the_matrix(str_key_word,alphabet);
+            fill_last_input(our_text);
+        }
+        else {
+            StringBuffer coded_str = new StringBuffer(coded_text.getText().toUpperCase().subSequence(0, coded_text.getText().length()));
+
+            delete_repeats(str_key_word,alphabet);
+            str_key_word = new StringBuffer(str_key_word.toString().replaceAll(" ","").subSequence(0,str_key_word.toString().replaceAll(" ","").length()));
+            alphabet = new StringBuffer(alphabet.toString().replaceAll(" ","").subSequence(0,alphabet.toString().replaceAll(" ","").length()));
+            fill_the_matrix(str_key_word,alphabet);
+            encode_it(coded_str);
 
         }
+
     }
 }
